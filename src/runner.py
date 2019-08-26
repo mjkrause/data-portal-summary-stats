c3178f721025a0e10e88ddb4c5a2b062a0c672ec
Michael Krause <michael.j.krause@gmail.com>
2019-08-26 11:56:55 -0700

    Removed condition to check whether AWS_DEFAULT_PROFILE env var is set

--- src/runner.py
+++ src/runner.py
@@ -35,46 +35,43 @@ def run_data_portal_summary_stats(args: argparse.Namespace):
     else:
         do_not_process = []
 
-    if os.getenv('AWS_DEFAULT_PROFILE') is None:
-        sys.exit('Environment variable AWS_DEFAULT_PROFILE not set - aborting.')
-    else:
-        logger.info(f'\nGenerating per-project summary statistics of matrix data from '
-                    f'{get_mss(**input_args).deployment} deployment environment.\n')
-        if args.source == 'fresh':
-            project_IDs = get_mss(**input_args).get_project_uuids_from_matrix_service()
-            project_info_azul = get_mss(**input_args).get_project_uuids_from_azul()
-            for project_ID in project_IDs:
-                logger.info(f'\nProcessing matrix from service with project ID {project_ID}')
-                if project_ID in do_not_process:
-                    logger.info(f'   ...skipping project ID {project_ID}')
-                    continue
-                project_title = get_project_title(project_info_azul, project_ID)
-                if project_title:
-                    logger.info(f'Project title: {project_title}')
-                else:
-                    logger.info(f'No project title found for project ID {project_ID} in Azul')
-                get_mss(**input_args).get_expression_matrix_from_service(project_ID)
-                get_mss(**input_args).write_response_content_to_file_and_unzip()
-                get_mss(**input_args).process_mtx_files()
-                get_mss(**input_args).create_images()
-                get_mss(**input_args).upload_figs_to_s3()
-                time.sleep(15)
-        elif args.source == 'canned':
-            mtx_files = get_mss(**input_args).get_canned_matrix_filenames_from_S3()
-            for mtx_file in mtx_files:
-                zipfile_ID = first(os.path.basename(mtx_file).split('.'))
-                if zipfile_ID in do_not_process:
-                    logger.info(f'\nSkipping processing of project ID {zipfile_ID}')
-                    continue
-                logger.info(f'\nDownloading canned matrix file with project ID {zipfile_ID} from S3.')
-                canned_file = get_mss(**input_args).download_canned_expression_matrix_from_S3(mtx_file)
-                if canned_file == []:
-                    continue
-                logger.info(f'Processing matrix with project ID {zipfile_ID} from S3')
-                get_mss(**input_args).write_response_content_to_file_and_unzip()
-                get_mss(**input_args).process_mtx_files()
-                get_mss(**input_args).create_images()
-                get_mss(**input_args).upload_figs_to_s3()
+    logger.info(f'\nGenerating per-project summary statistics of matrix data from '
+                f'{get_mss(**input_args).deployment} deployment environment.\n')
+    if args.source == 'fresh':
+        project_IDs = get_mss(**input_args).get_project_uuids_from_matrix_service()
+        project_info_azul = get_mss(**input_args).get_project_uuids_from_azul()
+        for project_ID in project_IDs:
+            logger.info(f'\nProcessing matrix from service with project ID {project_ID}')
+            if project_ID in do_not_process:
+                logger.info(f'   ...skipping project ID {project_ID}')
+                continue
+            project_title = get_project_title(project_info_azul, project_ID)
+            if project_title:
+                logger.info(f'Project title: {project_title}')
+            else:
+                logger.info(f'No project title found for project ID {project_ID} in Azul')
+            get_mss(**input_args).get_expression_matrix_from_service(project_ID)
+            get_mss(**input_args).write_response_content_to_file_and_unzip()
+            get_mss(**input_args).process_mtx_files()
+            get_mss(**input_args).create_images()
+            get_mss(**input_args).upload_figs_to_s3()
+            time.sleep(15)
+    elif args.source == 'canned':
+        mtx_files = get_mss(**input_args).get_canned_matrix_filenames_from_S3()
+        for mtx_file in mtx_files:
+            zipfile_ID = first(os.path.basename(mtx_file).split('.'))
+            if zipfile_ID in do_not_process:
+                logger.info(f'\nSkipping processing of project ID {zipfile_ID}')
+                continue
+            logger.info(f'\nDownloading canned matrix file with project ID {zipfile_ID} from S3.')
+            canned_file = get_mss(**input_args).download_canned_expression_matrix_from_S3(mtx_file)
+            if canned_file == []:
+                continue
+            logger.info(f'Processing matrix with project ID {zipfile_ID} from S3')
+            get_mss(**input_args).write_response_content_to_file_and_unzip()
+            get_mss(**input_args).process_mtx_files()
+            get_mss(**input_args).create_images()
+            get_mss(**input_args).upload_figs_to_s3()
 
 
 def mss_client():
